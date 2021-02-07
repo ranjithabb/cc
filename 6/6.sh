c206036160a46b171fc43ffb9d1b0724f3b70484
ranjithabb <55756207+ranjithabb@users.noreply.github.com>
2021-02-07 22:13:10 +0530

    Add files via upload

---
+++ 6/6.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+sudo apt-get install apache2 -y
+
+sudo systemctl start apache2
+
+sudo apt-get install php libapache2-mod-php php-mysql php-curl php-gd php-json php-zip php-mbstring -y
+
+sudo systemctl restart apache2
+
+sudo apt-get install mysql-server -y
+
+echo "done"
